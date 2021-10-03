0b55060958289ab50888eb3f94986118563596ac
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-10-04 02:07:45 +0530

    -

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const imdb = require('imdb-api')
+
 app.use(express.static(__dirname + '/public'));
+
 app.set('view engine','ejs')
 
 // -------------------------------
@@ -8,6 +11,9 @@ app.get("/",function(req,res)
 {
     res.render("home");
 })
+
+// imdb.get({name: 'The Toxic Avenger'}, {apiKey: 'foo', timeout: 30000}).then(console.log).catch(console.log);
+
 
 app.listen(3000, function()
 {
