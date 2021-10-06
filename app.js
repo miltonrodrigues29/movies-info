113c4fd0d90d58462e3bbb6ff7247d5e706bbe0a
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-10-06 20:21:27 +0530

    API Implementation complete

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
 const express = require('express')
 const app = express()
-var axios = require("axios").default;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const axios = require("axios");
+const bodyParser = require('body-parser')
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine','ejs')
 
 // -------------------------------
@@ -33,8 +13,36 @@ app.get("/",function(req,res)
     res.render("home");
 })
 
-// imdb.get({name: 'The Toxic Avenger'}, {apiKey: 'foo', timeout: 30000}).then(console.log).catch(console.log);
-    
+// post request
+app.post("/", function(req,res){
+    const movieName = req.body.movieName;
+    console.log(req)
+    const url = "https://www.omdbapi.com/?apikey=86f9dde7&t="+movieName;
+    axios.get(url).then(response => {
+
+        if(response.data.Response === "True"){
+            // res.render('movieInfo', {
+            //     movieTitle : response.data.Title, 
+            //     moviePlot : response.data.Plot, 
+            //     moviePoster : response.data.Poster,
+            //     imdbRating : typeof response.data.Ratings[0] === 'undefined'? "-/-" :response.data.Ratings[0].Value ,
+            //     rottenRating : typeof response.data.Ratings[1] === 'undefined'? "-/-" :response.data.Ratings[1].Value ,
+            //     metacriticRating: typeof response.data.Ratings[2] === 'undefined'? "-/-" :response.data.Ratings[2].Value ,
+            //     genre : response.data.Genre,
+            //     director : response.data.Director,
+            //     language : response.data.Language,
+            //     awards : response.data.Awards,
+            //     rated : response.data.Rated
+            // });
+            console.log(response.data);
+        } else{
+            res.render('error');
+        }
+
+        
+    })
+
+});
 
 app.listen(3000, function()
 {
